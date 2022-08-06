558aa9cb38b3b985b59fd8b3b236f5e48d37ac6e
Ubuntu <ubuntu@ip-172-31-11-93.ec2.internal>
2022-08-06 18:40:58 +0000

    changes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="lanrewadge/udacitymicroservice:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacitymicroservice\
+    --image=$dockerpath\
+    --port=80 --labels app=udacitymicroservice
 
 # Step 3:
 # List kubernetes pods
 
+
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacitymicroservice 8000:80
